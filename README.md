95f7cd553913a1e339626cd1dd67be872bb7ee23
LMXZ <1842227930@qq.com>
2024-01-11 18:10:07 +0800

    文档+实现

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # XXUST-Web-Authenticate
 某高校网络自动认证
+
+## 使用方法
+
+使用浏览器打开认证页面
+
+![](imgs/web-auth-page.png)
+
+按 `F12`，打开 `网络` 选项卡
+
+![](imgs/f12.png)
+
+弹出的界面右上角，点击网络设置（齿轮图标），勾选 `持续记录`
+
+![](imgs/f12-keep-record.png)
+
+输入用户名和密码，登录
+
+![](imgs/login.png)
+
+选择文件为 `do` 的项目
+
+在 `消息头` 中的 `请求头` 栏中打开 `原始` 选项，复制全文，粘贴至 `headers.txt` 中
+
+复制第一行 POST 后面的内容，粘贴至 `config.json` 中 `url` 字段
+
+![](imgs/copy-header.png)
+
+选择 `请求` 栏 `表单数据`，同样打开 `原始` 选项，复制全文，粘贴至粘贴至 `config.json` 中 `data` 字段
+
+运行 `auto-auth.py`
---
+++ auto-auth.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from time import sleep
+import random
+
+with open('./config.json', 'r') as file:
+    config = json.load(file)
+    url, data = config['url'], config['data']
+
+with open('./headers.txt', 'r') as file:
+    headersLoginTxt = file.read()
+
+while True:
+    try:
+        print('testing network...')
+        requests.get('https://www.baidu.com')
+        print('succeed!')
+        wait_time = 60+random.random() * 10
+        print(f'next test in {wait_time} seconds...')
+        sleep(wait_time)
+    except:
+        while True:
+            print('failed! trying to reconnect')
+
+            headersLogin = {k: v for k, v in map(lambda x: x.split(': '), headersLoginTxt.split('\n')[1:])}
+
+            resp = requests.post('http://222.197.192.59:9090/zportal/login/do', headers=headersLogin, data=data.format(username='201910416135', pwd='12wyw...').encode('ascii'))
+            try:
+                print('reconnect', json.loads(resp.content)['result'])
+                break
+            except:
+                print('reconnect failed')
+            
+            wait_time = 10+random.random() * 3
+            print(f'retry in {wait_time} seconds...')
+            sleep(wait_time)
